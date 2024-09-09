69687b98997977cace52126cf2fc3a9aa8a73328
Yoon321 <tkdwls734@gmail.com>
2024-09-09 10:12:02 +0900

    [feat] 프로젝트 셋팅

---
+++ src/main/java/com/example/hanghaemarket/HanghaeMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanghaemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeMarketApplication.class, args);
+    }
+
+}
